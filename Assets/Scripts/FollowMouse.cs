ad7ed1488d0861a59e017c0f2d0260719b685aa9
Felipe Sanchez Hancox <crazynoodles64@gmail.com>
2025-01-09 15:10:09 -0500

    Second Commit!

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -2,22 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstScript : MonoBehaviour
+public class FollowMouse : MonoBehaviour
 {
-    float speed = 0.000f;
-    
-
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
-        pos.x += speed;
-        transform.position = Input.mousePosition ;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
     }
 }
